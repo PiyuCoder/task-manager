c1d2ac154b4dc4edecb545a14ab7d5e7c30f3f62
PiyuCoder <piyushrai284@gmail.com>
2024-10-30 15:19:06 +0530

    path defined

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 dotenv.config();
 const connectDb = require("./config/db");
 connectDb();
